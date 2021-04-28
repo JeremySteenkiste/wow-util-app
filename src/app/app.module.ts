87b534e261ec5b84a3da527834a62549d739597a
Jérémy Steenkiste <jeremy.steenkiste.pro@gmail.com>
2021-04-28 23:03:05 +0200

    init angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccueilPageComponent } from './pages/accueil-page/accueil-page.component';
+import { OptimisationPageComponent } from './pages/optimisation-page/optimisation-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
+const materialImports = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatIconModule,
+];
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, AccueilPageComponent, OptimisationPageComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...materialImports,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
