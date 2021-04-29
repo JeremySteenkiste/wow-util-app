068bef013617b495a2e78e10ec9ff652854c806b
Jérémy.S <j.steenkiste@hotmail.com>
2021-04-29 10:12:38 +0200

    search perso
    Mise en place de la recherche d'un personnage

--- src/app/pages/accueil-page/accueil-page.component.ts
+++ src/app/pages/accueil-page/accueil-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonnageService } from 'src/app/services/personnage.service';
 
 @Component({
   selector: 'app-accueil-page',
   templateUrl: './accueil-page.component.html',
-  styleUrls: ['./accueil-page.component.scss']
+  styleUrls: ['./accueil-page.component.scss'],
 })
 export class AccueilPageComponent implements OnInit {
+  searchInput: string = '';
+  constructor(private personnageService: PersonnageService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  //TODO: Sélection du serveur de jeu
+  onSearch() {
+    this.personnageService
+      .searchPersonnage(this.searchInput, 'hyjal')
+      .subscribe((result) => {
+        console.log(result);
+      });
   }
-
 }
