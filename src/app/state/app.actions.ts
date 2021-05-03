91fb7bbf329dde6d3cf4e555a0e8015b0c46f36b
Jérémy.S <j.steenkiste@hotmail.com>
2021-05-03 19:45:18 +0200

    auth bnet

--- src/app/state/app.actions.ts
+++ src/app/state/app.actions.ts
@@ -1,4 +1,8 @@
 export namespace AppStateActions {
+  export class InitAction {
+    static readonly type = '[AppState] InitAction';
+    constructor() {}
+  }
   export class SearchAction {
     static readonly type = '[AppState] SearchAction';
     constructor(public nomPerso: string, public serveur: string) {}
